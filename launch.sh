6d3c2c05ab8234f6cc8252545b302ae3c86b573c
Jack Baron <jackmbaron@gmail.com>
2021-12-29 03:41:59 +0000

    add comments to launch script

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd Titanfall2
+
+# Setup environment
 export WINEARCH=win64
 export WINEPREFIX=/Wine
 export LIBGL_ALWAYS_SOFTWARE=1
@@ -7,11 +9,14 @@ export __GLX_VENDOR_LIBRARY_NAME=mesa
 export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/lvp_icd.x86_64.json
 export WINEDEBUG=-all
 
+# Ensure DXVK is installed
 winetricks dxvk || true
 
+# Download latest Northstar release if not found
 if [ ! -f "NorthstarLauncher.exe" ]; then
   curl -s https://api.github.com/repos/R2Northstar/Northstar/releases/latest | grep browser_download_url | cut -d '"' -f 4 | xargs wget -O northstar.zip
   unzip northstar.zip
 fi
 
+# Run Northstar
 xvfb-run bash -c "wine NorthstarLauncher.exe -dedicated -multiple | cat"
